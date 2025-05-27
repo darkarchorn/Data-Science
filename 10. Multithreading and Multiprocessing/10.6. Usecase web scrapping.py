b06ee03901ef54f6c02171708a9feaf14f41c373
darkarchorn <darkarchorn@gmail.com>
2025-05-27 17:01:03 +0700

    27/05/2025

---
+++ 10. Multithreading and Multiprocessing/10.6. Usecase web scrapping.py
@@ -0,0 +1,8 @@
+'''
+Real-world Example: Multithreading for I/O-bound Tasks
+Scenario: Web Scraping
+Web scraping often involves making numerous network requests to
+fetch web pages. These tasks are I/O-bound because they spend a lot of
+time waiting for responses fro servers. Multithreading can significantly
+improve the performance by allowing multiple web pages to be fetched concurrently
+'''
